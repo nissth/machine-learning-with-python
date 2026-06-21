db6a7d15f185f7f905e885cd764f2f0ea38084f0
Hayrunnisa Günay <152597580+nissth@users.noreply.github.com>
2026-06-21 16:21:45 +0300

    missing datas

---
+++ missing-datas.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#importing data
+datas = pd.read_csv('eksikveriler.csv')
+
+#print(datas)
+
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean') #puts the mean into the missing datas
+
+yas = datas.iloc[:,1:4].values
+print(yas)
+imputer= imputer.fit(yas[:,1:4])
+yas[:,1:4] = imputer.transform(yas[:,1:4])
+print(yas)
