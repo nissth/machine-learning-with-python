6408dd41008e218c4d946d9af718d11e45a62769
Hayrunnisa Günay <152597580+nissth@users.noreply.github.com>
2026-06-21 15:48:52 +0300

    reading csv

--- start.py
+++ start.py
@@ -2,7 +2,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-#importing datas
+#importing data
 datas = pd.read_csv('veriler.csv')
 
 print(datas)
+
+ulke = datas[['ulke']]
+print(ulke)
+
+boykilo = datas[['boy', 'kilo']]
+print(boykilo)
