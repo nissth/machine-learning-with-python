cde10a97e6002eb4b20c5c01493f5c9cf9269f2b
Hayrunnisa Günay <152597580+nissth@users.noreply.github.com>
2026-06-29 14:28:11 +0300

    polynomial regression

---
+++ prediction/polynomial-regression/polynomial-regression.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# importing data
+datas = pd.read_csv('prediction/polynomial-regression/maaslar.csv')
+
+x = datas.iloc[:,1:2]
+y = datas.iloc[:,2:]
+#print(x)
+#print(y)
+
+# linear regression
+from sklearn.linear_model import LinearRegression
+lr = LinearRegression()
+lr.fit(x,y) # learning function
+
+plt.scatter(x,y)
+plt.plot(x,lr.predict(x))
+# plt.show()
+
+# polynomial regression
+from sklearn.preprocessing import PolynomialFeatures
+pr = PolynomialFeatures(degree = 2) # increase of the degree, increases the reliability
+x_poly = pr.fit_transform(x)
+print(x_poly)
+
+# transform values to polynomial first then use linear regression with the polynomial values
+lr2 = LinearRegression()
+lr2.fit(x_poly,y)
+
+plt.scatter(x,y)
+plt.plot(x,lr2.predict(pr.fit_transform(x)))
+plt.show()
+
+# testing the result of the regressions
+print(lr.predict(pd.DataFrame([[11]], columns=x.columns)))
+print(lr.predict(pd.DataFrame([[6.6]], columns=x.columns)))
+
+print(lr2.predict(pr.fit_transform(pd.DataFrame([[11]], columns=x.columns))))
+print(lr2.predict(pr.fit_transform(pd.DataFrame([[6.6]], columns=x.columns))))
+
+
+# from sklearn.preprocessing import PolynomialFeatures
+# pr = PolynomialFeatures(degree = 4)
+# x_poly = pr.fit_transform(x)
+# print(x_poly)
+
+# lr2 = LinearRegression()
+# lr2.fit(x_poly,y)
+
+# plt.scatter(x,y)
+# plt.plot(x,lr2.predict(pr.fit_transform(x)))
+# plt.show()
