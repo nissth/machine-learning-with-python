961c88656e4811396d148feef180010034fd72d1
Hayrunnisa Günay <152597580+nissth@users.noreply.github.com>
2026-07-03 10:12:07 +0300

    naive bayes with gaussian nb

---
+++ classification/naive-bayes/naive-bayes.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# importing data
+datas = pd.read_csv('classification/veriler.csv')
+
+x = datas.iloc[:,1:4].values # independent values
+y = datas.iloc[:,4:].values # dependent value
+
+# splitting dataset into train and test
+from sklearn.model_selection import train_test_split
+
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.33,random_state=0)
+
+# standardization
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+# naive bayes 
+from sklearn.naive_bayes import GaussianNB
+gnb = GaussianNB()
+gnb.fit(x_train,y_train.ravel())
+y_pred = gnb.predict(x_test)
+
+print(y_pred)
+print(y_test)
+
+# confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,y_pred)
+print(cm)
+
+
