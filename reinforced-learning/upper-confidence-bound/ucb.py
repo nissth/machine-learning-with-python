1bb75214da9e1c8c479420a63681720b5c5baf7d
Hayrunnisa Günay <152597580+nissth@users.noreply.github.com>
2026-07-30 15:05:47 +0300

    ucb

--- reinforced-learning/upper-confidence-bound/ucb.py
+++ reinforced-learning/upper-confidence-bound/ucb.py
@@ -6,7 +6,44 @@ import matplotlib.pyplot as plt
 datas = pd.read_csv('reinforced-learning/Ads_CTR_Optimisation.csv')
 # print(datas)  
 
+import math 
 
+N = 10000
+d = 10
+
+#Ri(n)
+reward = [0] * d
+#Ni(n)
+clicks = [0] * d
+sum = 0
+chosen = []
+
+for n in range(0,N):
+    ad = 0
+    max_ucb = 0
+    for i in range(0,d):
+        if(clicks[i] > 0):
+            avg = reward[i] / clicks[i]
+            delta = math.sqrt((3/2) * math.log(n+1)/ clicks[i])
+            ucb = avg + delta
+        else: 
+            ucb = 1e400
+
+        if max_ucb < ucb:
+            max_ucb = ucb
+            ad = i
+
+    chosen.append(ad)
+    clicks[ad] = clicks[ad] + 1
+
+    r = datas.values[n,ad]
+    reward[ad] = reward[ad] + r
+    sum = sum + r
+
+print(sum)
+
+plt.hist(chosen)
+plt.show()
 
 
 
